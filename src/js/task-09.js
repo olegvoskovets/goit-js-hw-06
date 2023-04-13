d80334584de2e4a32651329cfc46be5c4a61cfb8
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-04-13 16:45:42 +0300

    9завдання

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,19 @@
+const body = document.body;
+const changeColorBtn = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+// console.log(body);
+const handkeClickColorBody = (event) => {
+  // event.preventDefault();
+  const colorBody = getRandomHexColor();
+  colorText.textContent = colorBody;
+  body.style.backgroundColor = `${colorBody}`;
+  // console.log(colorBody);
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColorBtn.addEventListener("click", handkeClickColorBody);
