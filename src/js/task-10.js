1281f987785ecb52d9b310e7bb82745a0726f586
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-04-13 21:43:21 +0300

    10завдання

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,63 @@
+const inputNamber = document.getElementsByTagName("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+
+const boxes = document.querySelector("#boxes");
+
+let countDiv = 0;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const weBrawDiv = (w_h) => {
+  const myDiv = document.createElement("div");
+  myDiv.classList.add("item-task10");
+  myDiv.style.width = `${w_h}px`;
+  myDiv.style.height = `${w_h}px`;
+  myDiv.style.backgroundColor = getRandomHexColor();
+
+  return myDiv;
+};
+
+const createBoxes = (amount) => {
+  let addDiv = [];
+
+  if (amount) {
+    let widthHeight = 30;
+
+    for (let i = 0; i < amount; i += 1) {
+      addDiv.push(weBrawDiv(widthHeight));
+      widthHeight += 10;
+    }
+
+    return addDiv;
+  }
+  return null;
+};
+
+const changeInput = (event) => {
+  countDiv = Number(event.currentTarget.value);
+};
+const addDivInt = () => {
+  const divAddBoxes = createBoxes(countDiv);
+  if (divAddBoxes) {
+    boxes.append(...divAddBoxes);
+  }
+};
+
+const destroyDivInt = () => {
+  const divRemove = document.querySelectorAll(".item-task10");
+  // divRemove.map((item) => item.remove());
+  for (const div of divRemove) {
+    div.remove();
+  }
+  countDiv = "";
+  inputNamber[0].value = "";
+};
+
+createBtn.addEventListener("click", addDivInt);
+inputNamber[0].addEventListener("input", changeInput);
+destroyBtn.addEventListener("click", destroyDivInt);
